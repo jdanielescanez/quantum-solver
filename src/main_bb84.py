8a7a2b4af05cee4c7028a20f20a18dbb12156674
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-07 18:45:00 +0000

    Starting BB84 Main Program

--- src/main_bb84.py
+++ src/main_bb84.py
@@ -3,5 +3,6 @@ import sys
 from bb84.bb84 import BB84
 
 if __name__ == "__main__":
-  bb84 = BB84(sys.argv)
+  token = sys.argv[1] if len(sys.argv) > 1 else None
+  bb84 = BB84(token)
   bb84.run()
