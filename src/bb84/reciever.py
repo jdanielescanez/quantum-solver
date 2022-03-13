c11630b573ec77447744c0e0068d3f9658dc6275
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-13 16:59:15 +0000

    Updating Receiver Class

--- src/bb84/reciever.py
+++ src/bb84/reciever.py
@@ -1,48 +1,11 @@
 
+from bb84.participant import Participant
 from qiskit import QuantumCircuit, assemble
 from numpy.random import rand, randint
 from bb84.bb84 import N_BITS
 from math import ceil
 
-class Reciever:
-  def __init__(self, name, original_bits_size):
-    self.name = name
-    self.original_bits_size = original_bits_size
-    self.values = None
-    self.axes = None
-    self.key = None
-    self.safe_key = False
-    self.otp = None
-
-  def set_axes(self, axes=None):
-    if axes == None:
-      self.axes = list(randint(2, size=self.original_bits_size))
-    else:
-      self.axes = axes
-
-  def show_values(self):
-    print('\n' + self.name, 'Values:')
-    print(self.values)
-
-  def show_axes(self):
-    print('\n' + self.name, 'Axes:')
-    print(self.axes)
-
-  def show_key(self):
-    print('\n' + self.name, 'Key:')
-    print(self.key)
-
-  def show_otp(self):
-    print('\n' + self.name, 'OTP:')
-    print(self.otp)
-
-  def remove_garbage(self, another_axes):
-    self.key = []
-    for i in range(self.original_bits_size):
-      if self.axes[i] == another_axes[i]:
-        self.key.append(self.values[i])
-    self.key
-
+class Reciever(Participant):
   def decode_quantum_message(self, message, density, backend):
     self.values = []
     for i, qc in enumerate(message):
@@ -58,20 +21,3 @@ class Reciever:
       else:
         self.values.append(-1)
     return message
-
-  def confirm_key(self, shared_size):
-    self.key = self.key[shared_size:]
-    self.safe_key = True
-
-  def generate_otp(self):
-    self.otp = []
-    for i in range(ceil(len(self.key) / N_BITS)):
-      bits_string = ''.join(map(str, self.key[i * N_BITS: (i + 1) * N_BITS]))
-      self.otp.append(int(bits_string, 2))
-
-  def decode_otp_message(self, encoded_message):
-    decoded_message = ''
-    CHR_LIMIT = 1114112
-    for i, char in enumerate(encoded_message):
-      decoded_message += chr((ord(char) ^ self.otp[i % len(self.otp)]) % CHR_LIMIT)
-    return decoded_message
