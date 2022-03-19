e946718a57934dc8e6c5b071009745e1659fdb48
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-19 19:42:03 +0000

    Adding Main Menu Page

--- quantum_solver_web/src/pages/MenuPage.tsx
+++ quantum_solver_web/src/pages/MenuPage.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export function MenuPage() {
   return (
     <div>
-      <h1>Menu PAGE!!!</h1>
+      <h1>Main Menu</h1>
+      <Link to="/menu/backend">
+        <button type="button">
+          Select Backend
+        </button>
+      </Link>
+      <Link to="/menu/algorithm">
+        <button type="button">
+          Select Algorithm
+        </button>
+      </Link>
+      <Link to="/menu/params">
+        <button type="button">
+          Select Params
+        </button>
+      </Link>
+      <Link to="/menu/loading">
+        <button type="button">
+          Run QuantumSolver
+        </button>
+      </Link>
     </div>
   )
 }
