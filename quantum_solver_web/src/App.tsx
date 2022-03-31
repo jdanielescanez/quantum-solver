bb98aacf8a39837d7547351d3720f8bee6ccc4ae
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-31 22:28:51 +0100

    Changing triangles id

--- quantum_solver_web/src/App.tsx
+++ quantum_solver_web/src/App.tsx
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <div id='bg-image'></div>
-      <div id='box'></div>
-      <div id='box2'></div>
+      <div id='triangle'></div>
+      <div id='triangle2'></div>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/token' element={<TokenPage />} />
