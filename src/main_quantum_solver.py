a53ecf962de32b4ea3ef293149c53f0524fe0b95
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-05-08 19:48:12 +0100

    Improving QuantumSolver main program documentation

--- src/main_quantum_solver.py
+++ src/main_quantum_solver.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Author: J. Daniel Escánez
+# Ingeniería Informática - Universidad de La Laguna
+# Trabajo Final de Grado: QuantumSolver
+
+## QuantumSolver Main Program
 
 import sys
 import signal
