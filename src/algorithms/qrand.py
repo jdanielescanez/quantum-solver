e3d189cc9ad332b5c2ec42c1eade413c2301bfe7
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-05-08 19:46:03 +0100

    Improving QRand documentation

--- src/algorithms/qrand.py
+++ src/algorithms/qrand.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: J. Daniel Escánez
+# Ingeniería Informática - Universidad de La Laguna
+# Trabajo Final de Grado: QuantumSolver
 
 from qiskit import QuantumCircuit
 from algorithms.qalgorithm import QAlgorithm
 
+## A Quantum Algorithm to obtain random numbers for QuantumSolver
 class QRand(QAlgorithm):
+  ## Constructor
   def __init__(self):
     self.name = 'QRand'
     self.description = 'Gives a random number between 0 and 2 ^ n_qubits - 1'
@@ -16,6 +23,7 @@ class QRand(QAlgorithm):
     self.parse_result = lambda counts: int(list(counts.keys())[0], 2)
     self.parse_parameters = lambda parameters: [int(parameters[0])]
 
+  ## Verify that the parameter is the number of qubits to use
   def check_parameters(self, parameters):
     if len(parameters) == 1 and type(parameters[0]) == str:
       try:
@@ -23,6 +31,7 @@ class QRand(QAlgorithm):
       except:
         return False
 
+  ## Create the circuit
   def circuit(self, n=1):
     # Create a Quantum Circuit
     circuit = QuantumCircuit(n, n)
